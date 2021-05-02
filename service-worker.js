cccbdfb418120c736d23c9cd276ad3fb8a587de5
solvedDev <solvedDev@users.noreply.github.com>
2021-05-02 22:19:05 +0000

    Deploying to gh-pages from  @ 41610d2da248b1767a0f03e932d07add458cf50c 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nightly/precache-manifest.30d7babd729ce2160c83d311c214ffd4.js"
+  "/nightly/precache-manifest.80bb77fa251fcfa9827e03198fb976b0.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "bridge"});
