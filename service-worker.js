6029e2e3f170555c04ef030950bd60c4afca7d4c
7dev7urandom <7dev7urandom@users.noreply.github.com>
2021-08-10 23:38:39 +0000

    Deploying to gh-pages from  @ dfcb106116e82c6bfb7f5702eefae701a45620ee 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0b55d1ae498a389fc02ea296985b98ab.js"
+  "/precache-manifest.a23b5b5f51178ebc92b4ad3a0a4ecb6e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "bridge"});
